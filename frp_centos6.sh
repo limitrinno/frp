f4d657bae51810128920a0ca827691a771d0f679
limitrinno <limitrinno@gmail.com>
2019-04-05 11:12:31 +0800

    Update frp_centos6.sh

--- frp_centos6.sh
+++ frp_centos6.sh
@@ -1,6 +1,10 @@
-yum -y install wget
 wget https://github.com/fatedier/frp/releases/download/v0.25.3/frp_0.25.3_linux_amd64.tar.gz
 yum -y install tar
 tar -zxvf frp_0.25.3_linux_amd64.tar.gz
 mv frp_0.25.3_linux_amd64.tar.gz frp
 cd frp
+mv frps.ini frps.ini.back
+wget https://github.com/limitrinno/frp/blob/master/frps.ini
+yum -y install screen
+screen -S frp
+sh /root/frp/frps.exe -c /root/frp/frps.ini
